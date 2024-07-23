61ae854e7fdee654c2a72d2eac06352e201c1ecb
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2024-07-23 22:21:50 +0530

    practice projects page made

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const navel = document.getElementById('navbar');
-window.addEventListener('scroll',()=>{
-    if(window.scrollY >=500)
-    {
-        navel.classList.add('navbar-scrolled');
-    }
-    else if(window.scrollY < 500)
-    {
-        navel.classList.remove('navbar-scrolled');
-    }
-})
+// const navel = document.getElementById('navbar');
+// window.addEventListener('scroll',()=>{
+//     if(window.scrollY >=500)
+//     {
+//         navel.classList.add('navbar-scrolled');
+//     }
+//     else if(window.scrollY < 500)
+//     {
+//         navel.classList.remove('navbar-scrolled');
+//     }
+// })
 var typed = new Typed(".auto-type",{
     strings : ["Programmer", "Web Developer", "App Developer"],
     typeSpeed : 150,
